69b09eec2ea1a06ed78adc92a42c5268ce473f30
Vova iz Tambova <121602490+Vova-iz-Tambova@users.noreply.github.com>
2023-03-28 06:27:58 +0000

    Pending changes exported from your codespace

--- index.js
+++ index.js
@@ -5,23 +5,31 @@ const formElement = document.querySelector('.profile__profile-info');
 const nameInput = formElement.querySelector('.profile__name');
 const jobInput = formElement.querySelector('.profile__status');
 
-const editOn = function () {editElement.classList.add('edit_active');};
-const editOff = function () {editElement.classList.remove('edit_active');};
-editProfileButtonElement.addEventListener('click', editOn);
-editCloseButtonElement.addEventListener('click', editOff);
 
-const editProName = editElement.querySelector('.proName');
-const editProJob = editElement.querySelector('.proJob');
+//открытие окна
+const editOn = function () {
 editProName.value = nameInput.textContent;
 editProJob.value = jobInput.textContent;
+editElement.classList.add('edit_active');};
+editProfileButtonElement.addEventListener('click', editOn);
+
+// закрытие окна
+const editOff = function () {editElement.classList.remove('edit_active');};
+editCloseButtonElement.addEventListener('click', editOff);
+
+const editProName = editElement.querySelector('.edit__proname');
+const editProJob = editElement.querySelector('.edit__projob');
+
 
 const formElementSubmit = editElement.querySelector('.edit__submit');
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
-  nameInput.textContent = editProName;
-  editOff;
+  nameInput.textContent = editProName.value;
+  jobInput.textContent = editProJob.value;
+  editOff();
 }
 
+// отправка формы
 formElementSubmit.addEventListener('click', handleFormSubmit);
-console.log(nameInput, jobInput, formElementSubmit);
+
