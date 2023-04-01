7a414ff445672ea551fcf65a85b4acbd9a706a00
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-04-01 08:58:17 +0300

    hotfix 3

--- scripts/index.js
+++ scripts/index.js
@@ -4,29 +4,29 @@ const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const profileElement = document.querySelector('.profile__profile-info');
 const nameVar = profileElement.querySelector('.profile__name');
 const jobVar = profileElement.querySelector('.profile__status');
-const popupProName = popupElement.querySelector('.popup__form_proname');
-const popupProJob = popupElement.querySelector('.popup__form_projob');
-const formElementSubmit = popupElement.querySelector('.popup__submit');
+let popupProName = popupElement.querySelector('.popup__form_proname');
+let popupProJob = popupElement.querySelector('.popup__form_projob');
+const formElementSubmit = popupElement.querySelector('.popup__form');
 
 //открытие окна
-const popupOn = function () {
+const showPopup = function () {
   popupProName.value = nameVar.textContent;
   popupProJob.value = jobVar.textContent;
   popupElement.classList.add('popup_opened');
 };
 // закрытие окна
-const popupOff = function () { popupElement.classList.remove('popup_opened'); };
+const hidePopup = function () { popupElement.classList.remove('popup_opened'); };
 
-function handleFormSubmit(evt) {
-  evt.preventDefault();
+function handleFormSubmit(event) {
+  event.preventDefault();
   nameVar.textContent = popupProName.value;
   jobVar.textContent = popupProJob.value;
-  popupOff();
+  hidePopup();
 }
 
-popupProfileButtonElement.addEventListener('click', popupOn);
-popupCloseButtonElement.addEventListener('click', popupOff);
+popupProfileButtonElement.addEventListener('click', showPopup);
+popupCloseButtonElement.addEventListener('click', hidePopup);
 
 // отправка формы
-formElementSubmit.addEventListener('click', handleFormSubmit);
+formElementSubmit.addEventListener('submit', handleFormSubmit);
 
