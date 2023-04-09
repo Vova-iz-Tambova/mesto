0c9e9dc7459afac5206476d40c251082c436d0c2
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-04-09 12:05:45 +0300

    add template

---
+++ scripts/cards.js
@@ -0,0 +1,42 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardTemplate = document.querySelector('.card');
+
+const createCardElement = (cardData) => {
+  const cardElement = cardTemplate.content
+  .querySelector('.card')
+  .cloneNode(true);
+  console.log(cardElement);
+  //console.log(cardData);
+};
+
+initialCards.forEach((card) => {
+  createCardElement(card);
+});
+
+
