01eda609b74c2ad6c1db1e8e42f5e9732e1f1437
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-04-09 16:31:19 +0300

    active buttons delete like

--- scripts/cards.js
+++ scripts/cards.js
@@ -26,17 +26,41 @@ const initialCards = [
 ];
 
 const cardTemplate = document.querySelector('.card');
+const cardGrid = document.querySelector('.elements');
 
 const createCardElement = (cardData) => {
   const cardElement = cardTemplate.content
-  .querySelector('.card')
-  .cloneNode(true);
-  console.log(cardElement);
-  //console.log(cardData);
+    .querySelector('.elements__element')
+    .cloneNode(true);
+
+  const cardName = cardElement.querySelector('.elements__tag');
+  const cardPhoto = cardElement.querySelector('.elements__photo');
+
+  cardName.textContent = cardData.name;
+  cardPhoto.src = cardData.link;
+  cardPhoto.alt = cardData.name;
+
+  const deleteCardButton = cardElement.querySelector('.card__delete-button_type_delete');
+  const likeCardButton = cardElement.querySelector('.elements__like_type_like');
+
+  handleDelete = () => {
+    cardElement.remove();
+  };
+
+  handleLike = () => {
+    likeCardButton.classList.toggle('elements__like_active');
+  };
+
+  deleteCardButton.addEventListener('click', handleDelete);
+  likeCardButton.addEventListener('click', handleLike);
+
+  return cardElement;
+};
+
+const addCardElement = (cardElement) => {
+  cardGrid.prepend(cardElement);
 };
 
 initialCards.forEach((card) => {
-  createCardElement(card);
+  addCardElement(createCardElement(card));
 });
-
-
