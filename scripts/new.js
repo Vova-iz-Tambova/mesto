1ef43b92e2bf155610a11d73055461edbb5e8f54
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-06-16 14:24:08 +0300

    check

---
+++ scripts/new.js
@@ -0,0 +1,48 @@
+class Section {
+  constructor(items, renderer) {
+
+  }
+  addItem() {
+
+  }
+}
+
+class Popup {
+  constructor(popup) {
+    this._popup = popup
+  }
+  _handleEscClose = () => {
+
+  }
+  setEventListeners = () => {
+
+  }
+
+}
+
+class PopupWithImage extends Popup {
+  constructor(popup) {
+    super(popup)
+  }
+  _getInputValues = () => {
+
+  }
+  setEventListeners = () => {
+
+  }
+
+  close = () => {
+
+  }
+}
+
+class UserInfo {
+
+  getUserInfo = () => {
+
+  }
+
+  setUserInfo = () => {
+
+  }
+}
