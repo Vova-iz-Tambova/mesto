9ff3df68afe1f1b8071c5455ac8b1fc6c3173838
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-06-26 16:02:55 +0300

    frontend

---
+++ src/components/Api.js
@@ -0,0 +1,19 @@
+class Api {
+  constructor(options) {
+    // тело конструктора
+  }
+
+  getInitialCards() {
+    // ...
+  }
+
+  // другие методы работы с API
+}
+
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-42',
+  headers: {
+    authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
+    'Content-Type': 'application/json'
+  }
+});
