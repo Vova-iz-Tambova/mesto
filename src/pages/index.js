0a3c8485fe7528cfccc7732a2d9dd6163d953e44
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-06-27 10:17:42 +0300

    change avatar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 // import './index.css'
-//===============================================
+//==============================================================================================
 import Api from '../components/Api.js';
 import Card from '../components/Card.js'
 import FormValidator from '../components/FormValidator.js'
@@ -7,10 +7,11 @@ import Section from '../components/Section.js'
 import PopupWithImage from '../components/PopupWithImage.js'
 import PopupWithForm from '../components/PopupWithForm.js'
 import UserInfo from '../components/UserInfo.js'
-//===============================================
+//==============================================================================================
 import { params } from '../utils/constants.js'
-//===============================================
-// инициализация подключения api сервера
+//==============================================================================================
+// Инициализация подключения api сервера
+//==============================================================================================
 const api = new Api({
   baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-69',
   headers: {
@@ -18,6 +19,9 @@ const api = new Api({
     'Content-Type': 'application/json'
   }
 })
+//==============================================================================================
+// РАБОТА С ПРОФИЛЕМ
+//==============================================================================================
 // селекторы данных профиля
 const userInfo = new UserInfo({
   profileName: '.profile__name',
@@ -25,8 +29,57 @@ const userInfo = new UserInfo({
   profileAvatar: '.profile__avatar'
 })
 // загрузка данных профиля с сервера
-api.getUserInfo().then((data) => {userInfo.setUserInfo(data)})
-//===============================================
+api.getUserInfo().then((data) => {userInfo.setUserInfo(data)}).catch((err) => {console.log(err)})
+
+// Создания копии класса для редактирования профиля
+const profilePopupWhithForm = new PopupWithForm({
+  popupSelector: '.edit-profile',
+  handleFormSubmit: (input) => {
+    const data = {
+      profileName: input['profileName'],
+      profileStatus: input['profileStatus']
+    }
+    userInfo.setUserInfo(data)
+    profilePopupWhithForm.close()
+  }
+})
+profilePopupWhithForm.setEventListeners() // включение его слушателей
+// Создания копии класса для смены аватара и включение его слушателей
+const avatarPopupWhithForm = new PopupWithForm({
+  popupSelector: '.avatar-change',
+  handleFormSubmit: (input) => {
+    const data = {
+      profileAvatar: input['profileAvatar'],
+    }
+    document.querySelector('.profile__avatar').src = data.profileAvatar
+    avatarPopupWhithForm.close()
+  }
+})
+avatarPopupWhithForm.setEventListeners() // включение слушателей
+
+// Создание копии класса валидации редактирования профиля
+const editProfilePopupValidate = new FormValidator(params, '.edit-profile')
+editProfilePopupValidate.enableValidation() // включение валидации формы редактирования профиля
+
+// Создание копии класса валидации для формы смены аватара
+const avatarPopupValidate = new FormValidator(params, '.avatar-change')
+avatarPopupValidate.enableValidation() // включение валидации формы смены аватара
+
+// Слушатель для открытия формы редактирования профиля
+document.querySelector('.profile__edit-button').addEventListener('click', () => {
+  profilePopupWhithForm.setInputValues(userInfo.getUserInfo())
+  editProfilePopupValidate.disableButton()
+  profilePopupWhithForm.open()
+})
+
+// Слушатель для открытия формы смены аватара
+document.querySelector('.profile__avatar-button').addEventListener('click', () => {
+  avatarPopupValidate.disableButton()
+  avatarPopupWhithForm.open()
+})
+//==============================================================================================
+// РАБОТА С КАРТОЧКАМИ
+//==============================================================================================
 // открытие фото на весь экран
 const openfullScreenImage = new PopupWithImage({ popupSelector: '.fullscreen' })
 openfullScreenImage.setEventListeners()
@@ -44,30 +97,7 @@ const createCard = (data) => {
 // разметка карточки в секцию
 const section = new Section({renderer: (cardData) => createCard(cardData)},'.elements')
 // загрузка карточек с сервера
-api.getInitialCards().then((data) => {section.renderer(data)})
-// создания класса редактирования профиля
-const profilePopupWhithForm = new PopupWithForm({
-  popupSelector: '.edit-profile',
-  handleFormSubmit: (input) => {
-    const data = {
-      profileName: input['profileName'],
-      profileStatus: input['profileStatus']
-    }
-    userInfo.setUserInfo(data)
-    profilePopupWhithForm.close()
-  }
-})
-// обработка событий открытия окна редактирования профиля
-document.querySelector('.profile__edit-button').addEventListener('click', () => {
-  profilePopupWhithForm.setInputValues(userInfo.getUserInfo())
-  editProfilePopupValidate.disableButton()
-  profilePopupWhithForm.open()
-})
-// слушатели попапа редактирования профиля
-profilePopupWhithForm.setEventListeners()
-// включение валидации редактирования профиля
-const editProfilePopupValidate = new FormValidator(params, '.edit-profile')
-editProfilePopupValidate.enableValidation()
+api.getInitialCards().then((data) => {section.renderer(data)}).catch((err) => {console.log(err)})
 // обработка данных формы создания карточки
 const cardPopupWhithForm = new PopupWithForm({
   popupSelector: '.new-card',
@@ -90,3 +120,4 @@ cardPopupWhithForm.setEventListeners()
 // включение валидации добавления карточки
 const newCardPopupValidate = new FormValidator(params, '.new-card')
 newCardPopupValidate.enableValidation()
+
